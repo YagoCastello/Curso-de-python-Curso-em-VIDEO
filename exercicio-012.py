015c0ac15175745b34fccc3144a6df0ec84daf25
Yago Castello Saloman <yagodealmeidacs@gmail.com>
2022-07-26 00:23:20 -0300

    Create exercicio-012.py

---
+++ exercicio-012.py
@@ -0,0 +1,3 @@
+x = float(input('Digite o valor do produto para descobrir o seu novo preço na nossa promoção de queima de estoque:'))
+y=x*0.95
+print(' O novo preço do produto é de {}{:.2f}{} reais'.format('\033[4;32;47m',y,'\033[m'))
