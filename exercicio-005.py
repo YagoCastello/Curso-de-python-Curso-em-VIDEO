9f8b887c42718a8ed31640815cfc8801905ba3de
Yago Castello Saloman <yagodealmeidacs@gmail.com>
2022-07-26 00:38:23 -0300

    Update exercicio-005.py

--- exercicio-005.py
+++ exercicio-005.py
@@ -1,5 +1,5 @@
-x=int(input('Digite um numero:'))
-n=x+1
-a=x-1
-cores={'limpa':'\033[m','azul':'\033[1;36m','vermelho':'\033[31m','magenta':'\033[35m'}
+x = int(input('Digite um numero:'))
+n = x + 1
+a = x - 1
+cores = {'limpa':'\033[m','azul':'\033[1;36m','vermelho':'\033[31m','magenta':'\033[35m'}
 print('O sucessor do numero {}{}{} é o {}{}{} e o seu antecessor é o {}{}{}'.format(cores['vermelho'],x,cores['limpa'],cores['magenta'],n,cores['limpa'],cores['azul'],a,cores['limpa']))
