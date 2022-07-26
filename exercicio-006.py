f14f81e1b4f7f9b913d08c04cd95d75ed209a551
Yago Castello Saloman <yagodealmeidacs@gmail.com>
2022-07-26 00:38:25 -0300

    Update exercicio-006.py

--- exercicio-006.py
+++ exercicio-006.py
@@ -1,4 +1,4 @@
 from math import sqrt
-x=int(input('Digite um numero :'))
-n=x**(1/2)
+x = int(input('Digite um numero :'))
+n = x**(1/2)
 print(' O dobro é : {}{}{}\n O triplo é {}{}{} \n E a raiz quadrada é {}{}{}'.format('\033[1;34;45m',x*2,'\033[m','\033[4;37;41m',x*3,'\033[m','\033[4;31;45m',sqrt(x),'\033[m'))
