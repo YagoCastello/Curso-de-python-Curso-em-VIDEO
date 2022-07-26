11b9d163fa9d15fea50e25b33a5fc3bfa887f50e
Yago Castello Saloman <yagodealmeidacs@gmail.com>
2022-07-26 00:23:22 -0300

    Create exercicio-013.py

---
+++ exercicio-013.py
@@ -0,0 +1,3 @@
+x=float(input('Digite o valor do seu salario para descobrir o valor do seu novo salario reajustado:'))
+y=x*1.15
+print('O seu novo salario é de {}{:.2f}{}'.format('\033[1;32;44m',y,'\033[m'))
