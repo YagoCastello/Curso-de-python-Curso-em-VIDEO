0260e108da150ccaef01d1ff1d374ff0302cf27d
Yago Castello Saloman <yagodealmeidacs@gmail.com>
2022-07-26 00:38:27 -0300

    Update exercicio-007.py

--- exercicio-007.py
+++ exercicio-007.py
@@ -1,6 +1,6 @@
-n1=float(input('Digite a nota da p1:'))
-n2=float(input('Digite a nora da p2:'))
-m=(n1+n2)/2
+n1 = float(input('Digite a nota da p1:'))
+n2 = float(input('Digite a nora da p2:'))
+m = (n1+n2)/2
 #print( 'A media do aluno(a) é {}{:.2f}{}'.format('\033[1;32;45m',m,'\033[m'))
 if m>=6:
     print('Você está {}{}{} '.format('\033[1;32m','Aprovado','\033[m'))
