d338a1f54dd73e37a0447105d3e2559508db0b4d
Yago Castello Saloman <yagodealmeidacs@gmail.com>
2022-07-26 00:23:13 -0300

    Create exercicio-010.py

---
+++ exercicio-010.py
@@ -0,0 +1,3 @@
+x = float(input('Digite quanto dinheiro voce tem em reais para saber o seu valor em dolares:'))
+k = x/3.27
+print('Voce tem {}{:.2f}{} dolar(es)'.format('\033[1;32;45m',k,'\033[m'))
