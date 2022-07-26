740882d4a46c79b3443fe71b02dfabf3535a22d4
Yago Castello Saloman <yagodealmeidacs@gmail.com>
2022-07-26 00:43:16 -0300

    Create teste.py

---
+++ exercicio-109-Versao-Professor-Guanabara/teste.py
@@ -0,0 +1,10 @@
+#versão Guanabara
+from ex110 import moeda2
+
+p = float(input('Digite o preço: R$ '))
+moeda2.resumo(p, 10, 15)
+
+# print(f'O dobro de {p} é {moeda2.dobro(p)}')
+# print(f'Aumentando 10 %, temos {moeda2.aumentar(p,10,True)}')
+# print(f'Diminuindo 13 %, temos {moeda2.diminuir(p,13,True)}')
+#
