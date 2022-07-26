f7d6119d167ed5c189102b07a9a4590fd8e99df9
Yago Castello Saloman <yagodealmeidacs@gmail.com>
2022-07-26 00:38:18 -0300

    Update exercicio-003.py

--- exercicio-003.py
+++ exercicio-003.py
@@ -1,4 +1,4 @@
-x=int(input("Digite um numero:"))
-y=int(input('Digite um outro numero:'))
-s=x+y
+x = int(input("Digite um numero:"))
+y = int(input('Digite um outro numero:'))
+s = x + y
 print('A soma entre {}{}{} e {}{}{} é igual a {}{}{}'.format('\033[1;31;42m',x,'\033[m','\033[4;33;45m',y,'\033[m','\033[7;34;42m',s,'\033[m'))
